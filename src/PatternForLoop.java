430be3e6ccde93fe5fb55a16e6bbb09039e2bf5a
Vaibhav Diwan <divaibhav@gmail.com>
2024-08-02 19:55:37 +0530

    for loop demo

---
+++ src/PatternForLoop.java
@@ -0,0 +1,16 @@
+public class PatternForLoop {
+    public static void main(String[] args) {
+        /*
+        *****
+        *****
+         */
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print("*");
+            }
+            //line change
+            System.out.println();
+
+        }
+    }
+}
